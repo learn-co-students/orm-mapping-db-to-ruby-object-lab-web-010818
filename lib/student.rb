a9cf7d23c0c019f19b1d22bf4ebc12473b5f6c1c
matjack9 <matjack9@gmail.com>
2018-01-16 15:26:18 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,27 +3,47 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students;
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+      LIMIT 1;
+    SQL
+
+    row = DB[:conn].execute(sql, name).flatten
+    self.new_from_db(row)
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
@@ -39,5 +59,26 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+
+  def self.count_all_students_in_grade_9
+    DB[:conn].execute("SELECT * FROM students WHERE grade = 9;")
+  end
+
+  def self.students_below_12th_grade
+    DB[:conn].execute("SELECT * FROM students WHERE grade < 12;")
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT ?;", num)
+  end
+
+  def self.first_student_in_grade_10
+    row = DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT 1;").flatten
+    self.new_from_db(row)
+  end
+
+  def self.all_students_in_grade_X(num)
+    DB[:conn].execute("SELECT * FROM students WHERE grade = ?;", num)
   end
 end
